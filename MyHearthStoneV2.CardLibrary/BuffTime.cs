f0f8efb6f6c6c1cd8793c7e4f3a4499035b6388e
drawde <drawde@126.com>
2017-07-09 14:24:18 +0800

    sx

---
+++ MyHearthStoneV2.CardLibrary/BuffTime.cs
@@ -0,0 +1,20 @@
+﻿using MyHearthStoneV2.CardEnum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyHearthStoneV2.CardLibrary
+{
+    public class BuffTime
+    {
+        public BuffTime(Type type, BuffTimeLimit time)
+        {
+            buffType = type;
+            timeLimit = time;
+        }
+        public Type buffType { get; set; }
+        public BuffTimeLimit timeLimit { get; set; }
+    }
+}
