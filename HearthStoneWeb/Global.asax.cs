e4c1948ffbef7dd8f4e96907b4e937ba7e51d31b
彭令禹 <drawde@126.com>
2017-07-10 09:25:06 +0800

    6

--- HearthStoneWeb/Global.asax.cs
+++ HearthStoneWeb/Global.asax.cs
@@ -12,6 +12,7 @@ namespace HearthStoneWeb
     {
         protected void Application_Start()
         {
+
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
