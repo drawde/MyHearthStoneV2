a9c1d32b73773ee483580a85b71ac40e3138eb72
彭令禹 <drawde@126.com>
2017-09-05 10:59:15 +0800

    修复心跳BUG

--- MyHearthStoneV2.Web/js/index.js
+++ MyHearthStoneV2.Web/js/index.js
@@ -1,1 +1,6 @@
-﻿
+﻿$(function () {
+    if (!!getUserCode()) {
+        $("#userLabel li a").html("欢迎回来，" + getUserName());
+        $("#visitorLabel").fadeOut(1000, function () { $("#userLabel").fadeIn(1000) });
+    }
+});
